997fdf6ddb189859e32367e2842d28c0d9be8643
ridiculouscircumstances <froxxanup@yandex.ru>
2023-05-14 21:32:35 +0700

    feat: added kong

--- main_service/routes/api.php
+++ main_service/routes/api.php
@@ -5,7 +5,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('oauth')->group(function() {
+    dump(request()->all());
     Route::get('/google', [OAuthLoginController::class, 'redirectToProvider'])->name('google');
     Route::get('/google/callback', [OAuthLoginController::class, 'handleProviderResponse'])->name('google');
-
+    Route::get('/test', function() {
+        return 'Yes we are';
+    });
 });
